eadafb8119799e275af28a0bbcdc4fa9240f9817
jogwen <joanna.wakeford+github@gmail.com>
2012-07-12 00:09:34 +0100

    Commit question 3 as wip

---
+++ euler3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+# What is the largest prime factor of the number 600851475143?
+
+number = 600851475143
+result = 0
+
+print "The largest prime factor of %d is %d" % (number, result)
